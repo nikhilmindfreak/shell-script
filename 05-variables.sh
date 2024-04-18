13ed6b08eed5c09d9214d57ab03b39d27efaaa47
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-18 19:29:33 -0400

    allow

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,9 +3,11 @@
 echo "please enetr username"
 
 read -s USERNAME  #here USERNAME is varioable, -s is used to hide whhat you write in linux
-
+# read is for storing 
 echo "please enter password::"
 
 read -s PASSWORD
 
 echo "username is : $USERNAME , password is : $PASSWORD"
+
+# short cut = read -p "enter username: " username
