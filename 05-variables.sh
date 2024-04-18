4c06468223ac92711f1278325f06a204f92b8839
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-18 18:14:03 -0400

    allow

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo "please enetr username"
+
+read USERNAME  #here USERNAME is varioable
+
+echo "please enter password::"
+
+read PASSWORD
+
+echo "username is : $USERNAME , password is : $"
