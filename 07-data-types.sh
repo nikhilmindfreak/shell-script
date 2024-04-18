7235d19a0c7863cec157dd671fac23c47d4dba25
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-18 19:12:51 -0400

    allow

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -6,3 +6,9 @@ NO2=$2
 SUM=$(($NO1+$NO2))
 
 echo "total of $NO1 and $NO2 is: $SUM"
+
+# we used variable $ here but we are passing the argument from outside 
+# we are doing arthamtic operators here
+# # when we $ the shell treats as an expression
+# $1 and $2 is the outside passing argument
+# if you give 0 and nikhil the shells reponds with answer 0 as it tell differnce in name and numbers
---
+++ 08-special-variannt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+echo "Process ID of last background command: $!"
+
+# $@ is the symobol to print all variables , only @ is used to print everything
+# $# is the symbol to print how many variables, # is nothing but notation of number
+# $0 is the symbole to print script name
+# $PWD is the sybol to print current directory
+# $home is the symbol to print home directory of current user
+# $USER is the symbol print which user is running this script
+# $HOSTNAME 
