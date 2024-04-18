97b5d8b30e42429d96f166fe785d23f0f4ba3bd5
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-18 18:38:00 -0400

    allow

--- 06-array.sh
+++ 06-array.sh
@@ -6,12 +6,12 @@ MOVIES=( "RRR" "DJ tIllu" "murari")
 
 # index are 0,1,2 = index always starts with 0 , index is always 1 size less
 
-echo "first move is" : ${MOVIES[0]}"
-
-echo "first move is" : ${MOVIES[1]}"
-
-echo "first move is" : ${MOVIES[@]}"
-
 # @ is used to includ all in the its everything
 
 # in varibales even if you dont give {} its fine but in array you have to give {} beacuse later we input []
+
+echo "first move is":${MOVIES[0]}"
+
+echo "first move is":${MOVIES[@]}"  
+
+echo "first move is":${MOVIES[0]}"
