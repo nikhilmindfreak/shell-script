4e39d1389f299642f8cfafae39a26549012a64c6
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-18 20:27:08 -0400

    allow

---
+++ 10-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u) # shell check condition the id and give to variable userid then write if condintion
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root acccess"
+else
+echo "you are super user"
+if
+
+
+dnf install mysql -y 
