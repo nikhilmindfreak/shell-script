61c6a16e93929e6b7c8874dd5cb84a76558f9edb
nikhilmindfreak <166666868+nikhilmindfreak@users.noreply.github.com>
2024-04-19 11:20:54 -0400

    Update 03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ person1=ram
 person2=sham
 
 echo $person1: hello $person1, how are you
-echo "$person2: i am sham , not ram"
-echo "$person1: hello $person1, how are you"
-echo "$person2: i am sham , not ram"
+echo $person2: i am sham , not ram
+echo $person1: hello $person1, how are you
+echo $person2: i am sham , not ram
