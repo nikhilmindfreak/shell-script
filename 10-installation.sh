890878eb9f0db38bc5365180eac8b8a9aa2777d1
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-18 20:42:06 -0400

    allow

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,8 +7,23 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root acccess"
+    exit 1 # we need write exit other than zero so write exit 1- 127 any number
 else
     echo "you are super user"
 fi
 
 dnf install mysql -y 
+
+# we should give a step here to check if the command is succes
+
+if [ $? -ne 0 ]  # $? is for to checkd previous command is success
+then
+    echo "Installion of mysql is failure"
+    exit 1 # we need write exit other than zero so write exit 1- 127 any number
+else
+    echo "Installion of mysql is success"
+fi
+
+
+echo " is script proceeding"
+
