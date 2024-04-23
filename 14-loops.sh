cfd1758737ae0fd0853f301f99ee5aa358551fa4
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-22 21:58:34 -0400

    a

---
+++ 14-loops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..20} # 2 dots is the syntax, # it will loop sfrom 1 to 20
+do     #loop starts  like flower brackets
+   echo $i
+done  # loop ends 
+
+# it will come out after 20 from loop
---
+++ 15-loops.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {1..100} # it will loop from 1 to 20
+do
+    echo $i
+done
