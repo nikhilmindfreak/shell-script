ba7ebdf6bd40d02527a783bffaa367bdacefc7b2
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-22 22:07:33 -0400

    a

--- 16-install-packages.sh
+++ 16-install-packages.sh
@@ -21,7 +21,7 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
+    echo -e "Please run this script with $G root access $N "
     exit 1 # manually exit if error comes.
 else
     echo "You are super user."
