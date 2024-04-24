8bcebcb4d5b8304f771950e8c1cc64bbcf1cde5e
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-23 21:17:59 -0400

    a

--- expense-shell/backend-idm.sh
+++ expense-shell/backend-idm.sh
@@ -8,7 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+echo "please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -48,3 +49,37 @@ then
 else
     echo -e "expense user already created..$Y SKIPPING $N"
 fi
+
+mkdir -p /app &>>$LOGFILE #p is used to create directory is name already exits error also doesnt show no need idempotency
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
+
+cd /app 
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+# manually next step is vim , but is for visual for human for shell script create .sh file backend service and cp the absoulte path
+
+cp /home/ec2-user/shell-script/expense-shell/backend-service /etc/systemd/system/backend-service &>>$LOGFILE
+VALIDATE $? "copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+systemctl start backend &>>$LOGFILE
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Validating- daemon, start-enable-backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Tnstalling Mysql client"
+
+mysql -h db.devopsme.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+#for above use read command and give variables on top beacuse we dont password in open sorce
+VALIDATE $? "Schema Loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting backend"
+
