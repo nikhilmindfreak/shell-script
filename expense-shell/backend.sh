e35c2163e19dfa81bc2daa01ccf56d922a8acecd
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-23 20:10:26 -0400

    a

---
+++ expense-shell/backend.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabiling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+useradd expense
+VALIDATE $? "Creating expense user"
+
+# if we repeat the scrpit we get error as we already have "expense user" so use idempotency here
+
+id expense
+if [ $? -ne 0 ]
+then 
+    useradd expense
+    VALIDATE $? "Creating expense user"
+else
+    echo -e " expense user already created..$Y SKIPPING $N"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+#Below code will be useful for idempotent nature
+mysql -h db.devopsme.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE  # we can use any command to check
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
