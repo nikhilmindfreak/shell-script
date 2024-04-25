9f34358556f452e9ac421dbb413f6196ebb3eeab
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-24 21:29:00 -0400

    a

---
+++ 19-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e  # replaced validate command set will automatically check all commands and exits if something is wrong
+
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR # to check
+
+USERID=$(id -u) #ERR
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysfaffql -y
+dnf install git -y
+
+echo "is script proceeding?"
