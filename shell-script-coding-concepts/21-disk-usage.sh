e1a4abb28bcd176fd2671f083bc7556a572fcfb3
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-04-25 21:28:29 -0400

    updated-common

--- shell-script-coding-concepts/21-disk-usage.sh
+++ shell-script-coding-concepts/21-disk-usage.sh
@@ -19,3 +19,7 @@ echo -e "Message: $MESSAGE"
 echo "$MESSAGE" | mail -s "Disk Usage Alert" nikhilmindfreak2727@gmail.com
 
 # echo "body" | mail -s "subject" to-address
+# Here
+# += is used befor message to print all the lines not to over write the when we use loop
+# /n & -e = is used for print lines in step wise not in one line
+# -e= to print message in steps not in one line
